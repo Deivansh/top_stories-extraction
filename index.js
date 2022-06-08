719c0efde7e1b307e54634a457c5753957fa3334
Devansh <devansh.sharma@kochartech.com>
2022-06-08 11:27:48 +0530

    Fix: Remove axios library

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const axios = require("axios").default;
 const port = 3000;
 
 app.get("/getTimeStories", async (req, res) => {
@@ -37,32 +36,6 @@ function getTimeStories() {
     })
     .catch((err) => new Error(err));
 }
-
-// return axios
-//   .get(url)
-//   .then(({ data }) => {
-//     if (!data) throw new Error("No data available.");
-//     const result = [];
-//     const li_href = /<li.*?class="latest-stories__item".*?>([\s\S]*?)<\/li>/gm;
-//     const href_regex = /"\/(.*?)"/gm;
-//     const heading_href =
-//       /<h3.*?class="latest-stories__item-headline".*?>.*?<\/h3>/gm;
-//     const htag_href = /<h3.*?>|<\/?h3>/g;
-//     const quotes_href = /"/g;
-
-//     const lists = data.match(li_href);
-
-//     lists.forEach((item) => {
-//       const link = url + item.match(href_regex)[0].replace(quotes_href, "");
-//       const title = item.match(heading_href)[0].replace(htag_href, "");
-//       result.push({
-//         title,
-//         link,
-//       });
-//     });
-//     return result;
-//   })
-//   .catch((err) => new Error(err));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
